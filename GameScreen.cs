066c3bc44778aaf5cc896dd1a3b2418cf5ba23fc
simon1013 <spgasino10@gmail.com>
2022-06-13 19:05:48 +0800

    Ilabyu

---
+++ GameScreen.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logo_Guesser
+{
+    [Activity(Label = "GameScreen")]
+    public class GameScreen : Activity
+    {
+        Button btnEnter, btnExit;
+        ImageView imgLogo;
+        TextView txtLogo;
+        EditText edt;
+        string[,] ReturnedVal;
+        string answer, entry;
+        Randomizer logoRnd = new Randomizer();
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.GameScreen_Layout);
+
+            btnExit = FindViewById<Button>(Resource.Id.button1);
+            btnEnter = FindViewById<Button>(Resource.Id.button2);
+            imgLogo = FindViewById<ImageView>(Resource.Id.imageView1);
+            txtLogo = FindViewById<TextView>(Resource.Id.textView1);
+            edt = FindViewById<EditText>(Resource.Id.editText1);
+
+            ParserClass myparser = new ParserClass("GameInfo.xml", "level");
+            myparser.ConnectXML();
+            ReturnedVal = myparser.ExtractXMLData("easy");
+
+            logoRnd.SetLogo(ReturnedVal, imgLogo);
+
+            logoRnd.GenerateRandomList();
+            answer = logoRnd.IterateRandomList();
+            btnEnter.Click += btnStartOnClick;
+        }
+
+        public void btnStartOnClick(object sender, EventArgs e)
+        {
+            entry = edt.Text.ToUpper().Trim();
+
+            if (answer == entry)
+            {
+                edt.Text = "";
+                answer = logoRnd.IterateRandomList();
+            }
+            else
+            {
+                edt.Text = "";
+                Toast.MakeText(Application.Context, $"{entry} is wrong", ToastLength.Short).Show();
+            }
+        }
+
+    }
+}
--- MainActivity.cs
+++ MainActivity.cs
@@ -4,17 +4,15 @@ using Android.Runtime;
 using AndroidX.AppCompat.App;
 using Android.Widget;
 using System;
+using Android.Content;
 
 namespace Logo_Guesser
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        Button btnStart;
-        ImageView imgLogo;
-        TextView txtLogo;
-        string[,] ReturnedVal;
-        Randomizer logoRnd = new Randomizer();
+        Button btn;
+    
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -22,22 +20,14 @@ namespace Logo_Guesser
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
-            btnStart = FindViewById<Button>(Resource.Id.button1);
-            imgLogo = FindViewById<ImageView>(Resource.Id.imageView1);
-            txtLogo = FindViewById<TextView>(Resource.Id.textView1);
+            btn = FindViewById<Button>(Resource.Id.button1);
 
-            ParserClass myparser = new ParserClass("GameInfo.xml", "level");
-            myparser.ConnectXML();
-            ReturnedVal = myparser.ExtractXMLData("easy");
-
-            logoRnd.SetLogo(ReturnedVal, imgLogo);
-            logoRnd.GenerateRandomList();
-
-            btnStart.Click += btnStartOnClick;
+            btn.Click += btnStartOnClick;
         }
         public void btnStartOnClick(object sender, EventArgs e)
         {
-            txtLogo.Text = logoRnd.IterateRandomList();
+            Intent i = new Intent(this, typeof(GameScreen));
+            StartActivity(i);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
