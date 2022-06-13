77c8d7af5eba0fdcd058b2c3129159d1640a02ba
jshalstr <joshaleisterv@gmail.com>
2022-06-13 14:17:57 +0800

    Randomizer, XML Parser, GameInfo.xml

---
+++ Randomizer.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace Logo_Guesser
+{
+    class Randomizer
+    {
+        private int interval;
+        private Timer timer;
+        private Random rnd;
+        private int index;
+        public string[,] XMLExtractedData;
+        public ImageView logo;
+        public string[] logoName;
+        public string[] imgPath;
+
+        public void SetLogo(string[,] par_XMLExtractedData)
+        {
+            XMLExtractedData = par_XMLExtractedData;
+        }
+
+        private void SelectRandomLogo(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            rnd = new Random();
+            index = rnd.Next(XMLExtractedData.GetLength(0));
+            int resourceId = (int)typeof(Resource.Drawable).GetField(XMLExtractedData[index, 2]).GetValue(null);
+            logo.SetImageResource(resourceId);
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ XML Parser.cs
@@ -0,0 +1,93 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+using Xamarin.Essentials;
+
+namespace Logo_Guesser
+{
+    internal class ParserClass
+    {
+        string XMLFileName, TagName;
+        Stream stream;
+        XmlDocument localInfo_Xml;
+        XmlNodeList tagList;
+        string[,] XMLExtractedValues;
+        int cnt;
+
+        public ParserClass(string xmlfilename, string tagname)
+        {
+            this.XMLFileName = xmlfilename;
+            this.TagName = tagname;
+        }
+
+        public async void ConnectXML()
+        {
+            try
+            {
+                stream = await FileSystem.OpenAppPackageFileAsync(XMLFileName);
+                localInfo_Xml = new XmlDocument();
+                localInfo_Xml.Load(stream);
+                tagList = localInfo_Xml.GetElementsByTagName(TagName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw ex;
+            }
+
+        }
+        public string[,] ExtractXMLData(string query_difficulty)
+        {
+            try
+            {
+                using (stream)
+                {
+                    int tagListCount = 0;
+                    foreach (XmlElement xmlElement in tagList)
+                    {
+                        string difficulty = xmlElement.ChildNodes[0].InnerText;
+                        string logoName = xmlElement.ChildNodes[1].InnerText;
+                        string imgPath = xmlElement.ChildNodes[2].InnerText;
+
+                        if (query_difficulty == difficulty)
+                        {
+                            XMLExtractedValues[cnt, 0] = difficulty;
+                            XMLExtractedValues[cnt, 1] = logoName;
+                            XMLExtractedValues[cnt, 2] = imgPath;
+                        }
+                    }
+                    XMLExtractedValues = new string[tagListCount, 3];
+                    foreach (XmlElement xmlElement in tagList)
+                    {
+                        string difficulty = xmlElement.ChildNodes[0].InnerText;
+                        string logoName = xmlElement.ChildNodes[1].InnerText;
+                        string imgPath = xmlElement.ChildNodes[2].InnerText;
+
+                        if (query_difficulty == difficulty)
+                        {
+                            XMLExtractedValues[cnt, 0] = difficulty;
+                            XMLExtractedValues[cnt, 1] = logoName;
+                            XMLExtractedValues[cnt, 2] = imgPath;
+                            cnt++;
+                        }
+                    }
+                }
+                return XMLExtractedValues;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw ex;
+            }
+        }
+    }
+}
